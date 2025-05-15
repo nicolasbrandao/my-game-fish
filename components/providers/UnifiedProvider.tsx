52c3553b5129d111fd4443fb16ff1fc36e5547e0
nicolas brandao <nicolas.brandaor@gmail.com>
2025-05-14 22:36:27 -0300

    fix: remove unused function

--- components/providers/UnifiedProvider.tsx
+++ components/providers/UnifiedProvider.tsx
@@ -1,10 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google"
-import "@/app/globals.css"
-import type { User } from "@/lib/types"
-import { getInitialUserSessionOnServer } from "@/lib/utils/auth/getInitialUserSessionOnServer"
-import { cookies } from "next/headers"
-import { Toaster } from "../ui/sonner"
-import { AuthProvider } from "./AuthProvider"
+import "@/app/globals.css";
+import type { User } from "@/lib/types";
+import { getInitialUserSessionOnServer } from "@/lib/utils/auth/getInitialUserSessionOnServer";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "../ui/sonner";
+import { AuthProvider } from "./AuthProvider";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -15,33 +14,6 @@ const geistMono = Geist_Mono({
 	variable: "--font-geist-mono",
 	subsets: ["latin"],
 })
-
-export async function getInitialUserSession(): Promise<User | null> {
-	const cookieStore = await cookies()
-	const token = cookieStore.get("access_token")?.value
-
-	if (!token) {
-		return null
-	}
-
-	const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
-	if (!backendUrl) {
-		return null
-	}
-	const response = await fetch(`${backendUrl}/auth/profile`, {
-		headers: {
-			Cookie: `access_token=${token}`,
-			"Content-Type": "application/json",
-		},
-		cache: "no-store",
-	})
-
-	if (response.ok) {
-		const userData: User = await response.json()
-		return userData
-	}
-	return null
-}
 
 export default async function UnifiedProvider({
 	children,
